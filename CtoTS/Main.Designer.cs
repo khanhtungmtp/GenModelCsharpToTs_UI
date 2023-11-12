85eba61d52fb25a890b0770c7dfc02a944d04e48
khanhtung97 <umlatao97@gmail.com>
2023-11-12 18:06:25 +0700

    first commit

---
+++ CtoTS/Main.Designer.cs
@@ -0,0 +1,187 @@
+﻿using ScintillaNET;
+
+namespace CtoTS
+{
+    partial class AppCToTS
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AppCToTS));
+            btnGen = new Button();
+            lbcsharp = new Label();
+            lbTypescript = new Label();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            inputTextBoxCsharp = new Scintilla();
+            inputTextBoxTypescript = new Scintilla();
+            btnCopy = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // btnGen
+            // 
+            btnGen.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btnGen.Location = new Point(342, 616);
+            btnGen.Name = "btnGen";
+            btnGen.Size = new Size(116, 47);
+            btnGen.TabIndex = 1;
+            btnGen.Text = "Convert TS";
+            btnGen.UseVisualStyleBackColor = true;
+            btnGen.Click += btnGen_Click;
+            // 
+            // lbcsharp
+            // 
+            lbcsharp.AutoSize = true;
+            lbcsharp.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            lbcsharp.Location = new Point(174, 102);
+            lbcsharp.Name = "lbcsharp";
+            lbcsharp.Size = new Size(38, 30);
+            lbcsharp.TabIndex = 5;
+            lbcsharp.Text = "C#";
+            // 
+            // lbTypescript
+            // 
+            lbTypescript.AutoSize = true;
+            lbTypescript.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            lbTypescript.Location = new Point(567, 102);
+            lbTypescript.Name = "lbTypescript";
+            lbTypescript.Size = new Size(106, 30);
+            lbTypescript.TabIndex = 6;
+            lbTypescript.Text = "Typescript";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(284, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(240, 30);
+            label1.TabIndex = 7;
+            label1.Text = "Convert C# to Typescript";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(359, 42);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(81, 66);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            // 
+            // inputTextBoxCsharp
+            // 
+            inputTextBoxCsharp.AutoCMaxHeight = 9;
+            inputTextBoxCsharp.Margins[0].Width = 20;
+            inputTextBoxCsharp.BiDirectionality = BiDirectionalDisplayType.Disabled;
+            inputTextBoxCsharp.LexerName = "cpp";
+            inputTextBoxCsharp.Location = new Point(3, 135);
+            inputTextBoxCsharp.Name = "inputTextBoxCsharp";
+            inputTextBoxCsharp.ScrollWidth = 1;
+            inputTextBoxCsharp.Size = new Size(392, 475);
+            inputTextBoxCsharp.TabIndents = true;
+            inputTextBoxCsharp.TabIndex = 0;
+            inputTextBoxCsharp.UseRightToLeftReadingLayout = false;
+            inputTextBoxCsharp.WrapMode = WrapMode.None;
+            // Assuming 'inputTextBox' is an instance of ScintillaNET.Scintilla
+            inputTextBoxCsharp.SetKeywords(0, "public class DateTime List DateOnly decimal int float double string void bool"); // Set your custom keywords
+
+            // Optionally, you can enable syntax highlighting
+            inputTextBoxCsharp.Styles[ScintillaNET.Style.Default].ForeColor = System.Drawing.Color.Silver;
+            inputTextBoxCsharp.Styles[ScintillaNET.Style.Cpp.Word].ForeColor = System.Drawing.Color.Blue;
+            inputTextBoxCsharp.Styles[ScintillaNET.Style.Cpp.Word2].ForeColor = System.Drawing.Color.Red;
+            // 
+            // inputTextBoxTypescript
+            // 
+            inputTextBoxTypescript.AutoCMaxHeight = 9;
+            inputTextBoxTypescript.Margins[0].Width = 20;
+            inputTextBoxTypescript.BiDirectionality = BiDirectionalDisplayType.Disabled;
+            inputTextBoxTypescript.LexerName = "cpp";
+            inputTextBoxTypescript.Location = new Point(401, 135);
+            inputTextBoxTypescript.Name = "inputTextBoxTypescript";
+            inputTextBoxTypescript.ScrollWidth = 1;
+            inputTextBoxTypescript.Size = new Size(402, 475);
+            inputTextBoxTypescript.TabIndents = true;
+            inputTextBoxTypescript.TabIndex = 0;
+            inputTextBoxTypescript.UseRightToLeftReadingLayout = false;
+            inputTextBoxTypescript.WrapMode = WrapMode.None;
+            // Assuming 'inputTextBox' is an instance of ScintillaNET.Scintilla
+            inputTextBoxTypescript.SetKeywords(0, "export interface any number int string void bool null Date boolean"); // Set your custom keywords
+
+            // Optionally, you can enable syntax highlighting
+            inputTextBoxTypescript.Styles[ScintillaNET.Style.Default].ForeColor = System.Drawing.Color.Silver;
+            inputTextBoxTypescript.Styles[ScintillaNET.Style.Cpp.Word].ForeColor = System.Drawing.Color.Blue;
+            inputTextBoxTypescript.Styles[ScintillaNET.Style.Cpp.Word2].ForeColor = System.Drawing.Color.Red;
+            // To hide the caret
+            inputTextBoxTypescript.CaretWidth = 0;
+            inputTextBoxTypescript.Click += inputTextBoxTypescript_Click;
+            // 
+            // btnCopy
+            // 
+            btnCopy.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnCopy.Location = new Point(731, 96);
+            btnCopy.Name = "btnCopy";
+            btnCopy.Size = new Size(71, 36);
+            btnCopy.TabIndex = 9;
+            btnCopy.Text = "Copy";
+            btnCopy.UseVisualStyleBackColor = true;
+            btnCopy.Click += btnCopy_Click;
+            // 
+            // AppCToTS
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(804, 673);
+            Controls.Add(btnCopy);
+            Controls.Add(inputTextBoxCsharp);
+            Controls.Add(inputTextBoxTypescript);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Controls.Add(lbTypescript);
+            Controls.Add(lbcsharp);
+            Controls.Add(btnGen);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "AppCToTS";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Csharp to Typescript";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Scintilla inputTextBoxCsharp;
+        private Scintilla inputTextBoxTypescript;
+        private Button btnGen;
+        private Label lbcsharp;
+        private Label lbTypescript;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Button btnCopy;
+    }
+}
---
+++ CtoTS/Main.cs
@@ -0,0 +1,277 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Text;
+
+
+namespace CtoTS
+{
+    public partial class AppCToTS : Form
+    {
+        public AppCToTS()
+        {
+            InitializeComponent();
+            btnCopy.Enabled = false;
+        }
+
+        private void btnGen_Click(object sender, EventArgs e)
+        {
+            // Lấy đường dẫn đến thư mục chứa tệp tin thực thi của ứng dụng
+            string assemblyLocation = System.AppContext.BaseDirectory;
+
+            // Tạo đường dẫn đầy đủ đến tệp tin tạm thời C#
+            string tempCSharpFilePath = Path.Combine(assemblyLocation, "TempAssembly", "TempCSharpFile.cs");
+
+            // Kiểm tra xem thư mục TempAssembly có tồn tại không
+            string tempAssemblyDirectory = Path.Combine(assemblyLocation, "TempAssembly");
+            if (!Directory.Exists(tempAssemblyDirectory))
+            {
+                Directory.CreateDirectory(tempAssemblyDirectory);
+            }
+
+            // Lấy nội dung từ RichTextBox
+            string cSharpCode = inputTextBoxCsharp.Text;
+
+            // Tạo một tạm thời C# source file
+            File.WriteAllText(tempCSharpFilePath, cSharpCode);
+
+            // Sử dụng Roslyn để biên dịch và đọc class C#
+            var syntaxTree = CSharpSyntaxTree.ParseText(cSharpCode);
+            var compilation = CSharpCompilation.Create("TempAssembly")
+                  .AddReferences(MetadataReference.CreateFromFile(typeof(object).Assembly.Location))
+                    .AddSyntaxTrees(syntaxTree);
+
+            var semanticModel = compilation.GetSemanticModel(syntaxTree);
+
+            var root = syntaxTree.GetRoot();
+            var classDeclaration = root.DescendantNodes().OfType<ClassDeclarationSyntax>().FirstOrDefault();
+            if (classDeclaration == null)
+            {
+                MessageBox.Show("Không tìm thấy class C# trong mã nguồn!");
+                return;
+            }
+
+            string className = classDeclaration.Identifier.Text;
+            CodeConverter codeConverter = new();
+            string tsInterface = codeConverter.ConvertCSharpClassToTypeScriptInterface(className, semanticModel.SyntaxTree.ToString());
+
+            // Lưu nội dung vào tệp tin .ts
+            string folderTypeScipt = Path.Combine(Directory.GetCurrentDirectory(), "Models");
+            CreateDirectoryIfNotExists(folderTypeScipt);
+            string outputTypescriptPath = Path.Combine(folderTypeScipt, $"{className.ToLower()}.ts");
+            File.WriteAllText(outputTypescriptPath, tsInterface);
+            inputTextBoxTypescript.Text = tsInterface;
+            if (inputTextBoxTypescript.Text != null)
+            {
+                btnCopy.Enabled = true;
+                btnCopy.Text = "Copy";
+            }
+            MessageBox.Show($"TypeScript interface saved to: {className.ToLower()}.ts", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void CreateDirectoryIfNotExists(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                try
+                {
+                    Directory.CreateDirectory(path);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error{ex.Message}", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void inputTextBoxTypescript_Click(object sender, EventArgs e)
+        {
+            // Check if the caret is at the beginning of the line
+            int currentLine = inputTextBoxTypescript.LineFromPosition(inputTextBoxTypescript.CurrentPosition);
+            int lineStart = inputTextBoxTypescript.LineFromPosition(currentLine);
+
+            if (inputTextBoxTypescript.CurrentPosition == lineStart)
+            {
+                // Hide the caret at the beginning of the line
+                inputTextBoxTypescript.CaretWidth = 0;
+            }
+            else
+            {
+                // Show the caret elsewhere
+                inputTextBoxTypescript.CaretWidth = 1;
+            }
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(inputTextBoxTypescript.Text);
+            btnCopy.Text = "Copied";
+        }
+    }
+
+    public class CodeConverter
+    {
+        public string ConvertToStandardPropertyNameTypeScript(string cSharpPropertyName)
+        {
+            StringBuilder result = new();
+            if (IsAllUpperCase(cSharpPropertyName))
+            {
+                // co 2 truong hop
+                /*
+                 1: khong co dau _   ex: MTP
+                 2: co dau _         ex: KHANHTUNG_MTP
+                */
+                if (cSharpPropertyName.Contains('_'))
+                {
+                    // Chuyển đổi chuỗi thành chữ thường và giữ nguyên chữ in hoa cuối cùng của phần truoc dấu '_' dau tien đến hết string
+                    string resultString = ConvertToLowerCaseWithUppercaseFirstAfterUnderscore(cSharpPropertyName);
+                    result.Append(resultString);
+                }
+                else
+                {
+                    result.Append(cSharpPropertyName.ToLower());
+                }
+            }
+            else
+            {
+                string resultString = ConvertFirstCharToLowerCase(cSharpPropertyName);
+                result.Append(resultString);
+            }
+
+
+            return result.ToString();
+        }
+        /// <summary>
+        /// // Chuyển đổi chuỗi thành chữ thường và giữ nguyên chữ in hoa cuối cùng của phần truoc dấu '_' dau tien đến hết string
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        static string ConvertToLowerCaseWithUppercaseFirstAfterUnderscore(string input)
+        {
+            string[] parts = input.Split('_');
+
+            if (parts.Length > 1)
+            {
+                parts[0] = ConvertLastCharToLowerCase(parts[0]);
+                // Ghép các phần lại thành chuỗi mới
+                return string.Join("_", parts);
+            }
+
+            return input;
+        }
+
+        static string ConvertLastCharToLowerCase(string input)
+        {
+            if (!string.IsNullOrWhiteSpace(input))
+            {
+                // Lấy chiều dài của chuỗi
+                int length = input.Length;
+
+                // Chuyển đổi chữ cái cuối cùng thành chữ thường
+                char lastChar = char.ToUpper(input[length - 1]);
+
+                // Tạo chuỗi mới bằng cách ghép chuỗi cũ và chữ cái cuối cùng đã chuyển đổi
+                return input.Substring(0, length - 1).ToLower() + lastChar;
+            }
+
+            return input;
+        }
+
+        static string ConvertFirstCharToLowerCase(string input)
+        {
+            if (!string.IsNullOrWhiteSpace(input))
+            {
+                // Lấy chiều dài của chuỗi
+                int length = input.Length;
+
+                // Chuyển đổi chữ cái dau tien thành chữ thường
+                char firstChar = char.ToLower(input[0]);
+
+                // Tạo chuỗi mới bằng cách ghép chuỗi cũ và chữ cái dau tien đã chuyển đổi
+                return firstChar + input.Substring(1, length - 1);
+            }
+
+            return input;
+        }
+
+        static bool IsAllUpperCase(string input)
+        {
+            return input == input.ToUpper();
+        }
+
+        public string ConvertCSharpTypeToTypeScript(string csharpType)
+        {
+            switch (csharpType)
+            {
+                case "int":
+                case "float":
+                case "double":
+                case "decimal":
+                    return "number";
+                case "int?":
+                case "float?":
+                case "double?":
+                case "decimal?":
+                    return "number | null";
+                case "string":
+                    return "string";
+                case "string?":
+                    return "string | null";
+                case "DateTime":
+                    return "string | Date";
+                case "DateTime?":
+                    return "string | Date | null";
+                case "bool":
+                    return "boolean";
+                default:
+                    if (csharpType.StartsWith("List<") && csharpType.EndsWith(">"))
+                    {
+                        // Extract the class name from List<className>
+                        string className = csharpType.Substring(5, csharpType.Length - 6);
+                        // Convert to TypeScript array notation
+                        return $"{className}[]";
+                    }
+                    return "any";
+            }
+        }
+
+        public string ConvertCSharpPropertyToTypeScript(string csharpPropertyName, string csharpType)
+        {
+            string tsPropertyName = ConvertToStandardPropertyNameTypeScript(csharpPropertyName);
+            string tsType = ConvertCSharpTypeToTypeScript(csharpType);
+
+            return $"{tsPropertyName}: {tsType};";
+        }
+
+        public string ConvertCSharpClassToTypeScriptInterface(string className, string csharpClass)
+        {
+            // Phân tách các dòng trong mã nguồn C#
+            string[] lines = csharpClass.Split('\n');
+
+            // Dùng List để lưu trữ các thuộc tính
+            List<string> tsProperties = new();
+
+            // Quét qua từng dòng mã nguồn C# để tìm thuộc tính
+            foreach (string line in lines)
+            {
+                // Sử dụng regex hoặc cách kiểm tra để xác định dòng có phải là khai báo thuộc tính
+                if (line.Contains("public") && line.Contains("{ get; set; }"))
+                {
+                    // Tìm tên thuộc tính và kiểu dữ liệu từ dòng khai báo
+                    string[] tokens = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    string csharpType = tokens[1];
+                    string csharpPropertyName = tokens[2].Replace("{", "").Trim();
+
+                    // Chuyển đổi thuộc tính C# sang TypeScript
+                    string tsProperty = ConvertCSharpPropertyToTypeScript(csharpPropertyName, csharpType);
+                    tsProperties.Add(tsProperty);
+                }
+            }
+
+            // Ghép các thuộc tính TypeScript lại thành interface
+            string tsInterface = $"export interface {className} {{\n\t{string.Join("\n\t", tsProperties)}\n}}";
+
+            return tsInterface;
+        }
+    }
+}
